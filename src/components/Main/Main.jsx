c7a8cdcadf551796374d922eaab5b67d2c61d4e7
Huang <h96716@gmail.com>
2024-03-24 20:52:34 +0800

    feat: add Main component with global styles

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,8 @@
+import styles from "./Main.module.scss";
+export default function Main() {
+  return (
+    <main class={`${styles.siteMain}`}>
+      <div class={`${styles.mainContainer}`}>Test</div>
+    </main>
+  );
+}
