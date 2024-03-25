75f0be1d420c9bffaf35213cae0a135fe224055a
Huang <h96716@gmail.com>
2024-03-25 17:47:14 +0800

    feat: add Step2 component and style

--- src/components/Main/Step/StepForm.jsx
+++ src/components/Main/Step/StepForm.jsx
@@ -1,9 +1,11 @@
 import styles from "./StepForm.module.scss";
-import Step1 from "./Form/Step1";
+// import Step1 from "./Form/Step1";
+import Step2 from "./Form/Step2";
 export default function StepForm() {
   return (
     <section className={`${styles.formContainer} col col-12`}>
-      <Step1 />
+      {/* <Step1 /> */}
+      <Step2 />
     </section>
   );
 }
