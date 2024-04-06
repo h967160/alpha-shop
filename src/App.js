caf67eb6c5d1ad21fb41791e77661dba84ee221d
Huang <h96716@gmail.com>
2024-04-06 16:53:11 +0800

    refactor: reorganize component structure in App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
 import Main from "./components/Main/Main";
-import StepContainer from "./components/Main/Step/StepContainer";
-import ProgressControl from "./components/Main/Step/ProgressControl";
-import Cart from "./components/Main/Cart/Cart";
 
 function App() {
   return (
     <div className="App">
-      <Main>
-        <StepContainer />
-        <Cart />
-        <ProgressControl />
-      </Main>
+      <Main />
     </div>
   );
 }
