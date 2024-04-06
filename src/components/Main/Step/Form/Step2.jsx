15e25fb57e29ff711427a4772900ce02eb2241c1
Huang <h96716@gmail.com>
2024-04-07 02:33:57 +0800

    fix: corrected shipping fee amounts in Step2 component

--- src/components/Main/Step/Form/Step2.jsx
+++ src/components/Main/Step/Form/Step2.jsx
@@ -8,9 +8,9 @@ export default function Step2() {
         <label className={`${styles.radioGroup} col col-12`} data-price="0">
           <input id="shipping-standard" type="radio" name="shipping" checked />
           <div className={`${styles.radioInfo}`}>
-            <div className="col col-12">
+            <div className={`${styles.infoControl} col col-12`}>
               <div className={`${styles.text}`}>標準運送</div>
-              <div className={`${styles.price}`}></div>
+              <div className={`${styles.price}`}>免費</div>
             </div>
             <div className={`${styles.period} col col-12`}>約 3~7 個工作天</div>
           </div>
@@ -19,9 +19,9 @@ export default function Step2() {
         <label className={`${styles.radioGroup} col col-12`} data-price="500">
           <input id="shipping-dhl" type="radio" name="shipping" />
           <div className={`${styles.radioInfo}`}>
-            <div className="col col-12">
+            <div className={`${styles.infoControl} col col-12`}>
               <div className={`${styles.text}`}>DHL 貨運</div>
-              <div className={`${styles.price}`}></div>
+              <div className={`${styles.price}`}>$500</div>
             </div>
             <div className={`${styles.period} col col-12`}>48 小時內送達</div>
           </div>
